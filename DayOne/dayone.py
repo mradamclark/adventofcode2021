b8bf73b4e32084a9448d213919b273b881776480
adam clark <mr.adam.clark@gmail.com>
2021-12-02 06:59:21 -0700

    rename files to be consistant

---
+++ DayOne/dayone.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python
+
+inputfile = 'day1_input.txt'
+
+
+increaseCount = 0
+depths = []
+
+with open(inputfile) as file:
+    depths = [int(line.strip()) for line in file]
+
+    prevDepth = depths[0]
+    for i in range(len(depths)):
+        currDepth = depths[i];
+            
+        if prevDepth < currDepth:
+            increaseCount = increaseCount + 1
+            
+        prevDepth = currDepth
+        
+
+print('Linear IncreaseCount = ' + str(increaseCount))
+        
+# Part 2 
+depths = []
+k = 3;
+increaseCount = 0
+
+with open(inputfile) as file:
+    depths = [int(line.strip()) for line in file]
+    
+prevDepthSum = sum(depths[:k])
+for i in range(len(depths) - k):
+    currDepthSum = prevDepthSum - depths[i] + depths[i+k]
+    if (prevDepthSum < currDepthSum):
+        increaseCount = increaseCount + 1
+    
+print('Windowed IncreaseCount = ' + str(increaseCount))
+        
+    
+        
