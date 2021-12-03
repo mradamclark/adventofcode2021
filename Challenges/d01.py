e84a208034b961b7f93cef15f2a449c95562be78
adam clark <mr.adam.clark@gmail.com>
2021-12-03 07:04:03 -0700

    making baby steps.

--- Challenges/d01.py
+++ Challenges/d01.py
@@ -1,39 +1,41 @@
 #!/usr/bin/python
 
-inputfile = '../Files/01.input.txt'
+def solve():
+    inputfile = '../Files/01.input.txt'
 
-increaseCount = 0
-depths = []
+    increaseCount = 0
+    depths = []
 
-with open(inputfile) as file:
-    depths = [int(line.strip()) for line in file]
+    with open(inputfile) as file:
+        depths = [int(line.strip()) for line in file]
 
-    prevDepth = depths[0]
-    for i in range(len(depths)):
-        currDepth = depths[i];
+        prevDepth = depths[0]
+        for i in range(len(depths)):
+            currDepth = depths[i]
+                
+            if prevDepth < currDepth:
+                increaseCount = increaseCount + 1
+                
+            prevDepth = currDepth      
+
+    print('Linear IncreaseCount = ' + str(increaseCount))
             
-        if prevDepth < currDepth:
+    # Part 2 
+    depths = []
+    k = 3;
+    increaseCount = 0
+
+    with open(inputfile) as file:
+        depths = [int(line.strip()) for line in file]
+        
+    prevDepthSum = sum(depths[:k])
+    for i in range(len(depths) - k):
+        currDepthSum = prevDepthSum - depths[i] + depths[i+k]
+        if (prevDepthSum < currDepthSum):
             increaseCount = increaseCount + 1
-            
-        prevDepth = currDepth      
-
-print('Linear IncreaseCount = ' + str(increaseCount))
         
-# Part 2 
-depths = []
-k = 3;
-increaseCount = 0
-
-with open(inputfile) as file:
-    depths = [int(line.strip()) for line in file]
-    
-prevDepthSum = sum(depths[:k])
-for i in range(len(depths) - k):
-    currDepthSum = prevDepthSum - depths[i] + depths[i+k]
-    if (prevDepthSum < currDepthSum):
-        increaseCount = increaseCount + 1
-    
-print('Windowed IncreaseCount = ' + str(increaseCount))
+    print('Windowed IncreaseCount = ' + str(increaseCount))
         
-    
+if __name__ == '__main__':
+    solve()
         
--- Challenges/d02.py
+++ Challenges/d02.py
@@ -1,43 +1,47 @@
 #!/usr/bin/python
 
-inputfile = '../Files/02.input.txt'
+def solve():
+    inputfile = '../Files/02.input.txt'
 
-position = 0
-depth = 0
+    position = 0
+    depth = 0
 
-with open(inputfile) as file:
-    commands = [line.strip() for line in file]
+    with open(inputfile) as file:
+        commands = [line.strip() for line in file]
 
-for i in range(len(commands)):
-    cmd_detail = commands[i].split(' ')
-    amount = int(cmd_detail[1])
-    if (cmd_detail[0] == 'forward'):
-        position += amount
-    elif (cmd_detail[0] == 'up'):
-        depth -= amount
-    elif (cmd_detail[0] == 'down'):
-        depth += amount
+    for i in range(len(commands)):
+        cmd_detail = commands[i].split(' ')
+        amount = int(cmd_detail[1])
+        if (cmd_detail[0] == 'forward'):
+            position += amount
+        elif (cmd_detail[0] == 'up'):
+            depth -= amount
+        elif (cmd_detail[0] == 'down'):
+            depth += amount
 
-print('Position:' + str(position) + ' Depth: ' + str(depth))
-print('Answer: ' + str(position * depth))
+    print('Position: ' + str(position) + ' Depth: ' + str(depth))
+    print('Answer: ' + str(position * depth))
 
-position = 0
-depth = 0
-aim = 0
+    position = 0
+    depth = 0
+    aim = 0
 
-with open(inputfile) as file:
-    commands = [line.strip() for line in file]
+    with open(inputfile) as file:
+        commands = [line.strip() for line in file]
 
-for i in range(len(commands)):
-    cmd_detail = commands[i].split(' ')
-    amount = int(cmd_detail[1])
-    if (cmd_detail[0] == 'forward'):
-        position += amount
-        depth += aim * amount
-    elif (cmd_detail[0] == 'up'):
-        aim -= amount
-    elif (cmd_detail[0] == 'down'):
-        aim += amount
+    for i in range(len(commands)):
+        cmd_detail = commands[i].split(' ')
+        amount = int(cmd_detail[1])
+        if (cmd_detail[0] == 'forward'):
+            position += amount
+            depth += aim * amount
+        elif (cmd_detail[0] == 'up'):
+            aim -= amount
+        elif (cmd_detail[0] == 'down'):
+            aim += amount
 
-print('Position:' + str(position) + ' Depth: ' + str(depth))
-print('Answer: ' + str(position * depth))
+    print('Position: ' + str(position) + ' Depth: ' + str(depth))
+    print('Answer: ' + str(position * depth))
+
+if __name__ == '__main__':
+    solve()
