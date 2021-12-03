a3f5a0bad0e61380aa5cf7c523c52d7ece924e91
adam clark <mr.adam.clark@gmail.com>
2021-12-02 23:56:11 -0700

    shorten the code some. only cause i am bored.

--- DayThree/daythree.py
+++ DayThree/daythree.py
@@ -2,74 +2,42 @@
 
 inputfile = 'puzzleInput.txt'
 
-def get_most_common_bit(position, bin_data, default_if_even):
-    total = len(bin_data)
+def count_ones(position, bin_data):
     ones = 0
     for s in bin_data:
         if s[position] == '1':
             ones += 1
-    zeros = total - ones
-    if (ones > zeros):
-        return '1'
-    elif ones < zeros:
-        return '0'
-    else:
-        return default_if_even
+    return ones
+             
+def get_common_bit(position, bin_data, most_common_result, least_common_result):
+    ones = count_ones(position, bin_data)
+    return most_common_result if ones >= (len(bin_data) - ones) else least_common_result     
     
-def get_least_common_bit(position, bin_data, default_if_even):
-    total = len(bin_data)
-    ones = 0
-    for s in bin_data:
-        if s[position] == '1':
-            ones += 1
-    zeros = total - ones
-    if (ones < zeros):
-        return '1'
-    elif ones > zeros:
-        return '0'
-    else:
-        return default_if_even
-        
-    
-def filter(getbit, default_if_even, bin_data, pos = 0):
+def filter(most_common_result, least_common_result, bin_data, pos = 0):
     if len(bin_data) == 1:
         return bin_data.pop(0)
     else:
         new_bin_data = []
-        bit = getbit(pos, bin_data, default_if_even)
+        bit = get_common_bit(pos, bin_data, most_common_result, least_common_result)
         for number in bin_data:
             if number[pos] == bit:
                 new_bin_data.append(number)
-        return filter(getbit, default_if_even, new_bin_data, pos + 1)
+        return filter(most_common_result, least_common_result, new_bin_data, pos + 1)
 
 with open(inputfile) as file:
     bin_data = [line.strip() for line in file]
-
-# totals = [0] * len(bin_data[0])
-# for line in bin_data:
-#     s = list(line)
-#     for i in range(len(s)):
-#         totals[i] += int(s[i])
-
-# for count in totals:
-#     if count < (len(bin_data) - count):
-#         gamma_rate += "0"
-#         epsilon_rate += "1"
-#     else:
-#         gamma_rate += "1"
-#         epsilon_rate += "0"
 
 gamma_rating = [] 
 epsilon_rating = [] 
       
 for i in range(len(bin_data[0])):
-    gamma_rating.append(get_most_common_bit(i, bin_data, '1'))
-    epsilon_rating.append(get_least_common_bit(i, bin_data, '0'))
+    gamma_rating.append(get_common_bit(i, bin_data, '1', '0'))
+    epsilon_rating.append(get_common_bit(i, bin_data, '0','1'))
     
-oxy_rating = int(filter(get_most_common_bit, '1', bin_data),2)
-co2_rating = int(filter(get_least_common_bit, '0', bin_data),2)
-
+oxy_rating = int(filter('1','0',bin_data),2)
+co2_rating = int(filter('0','1', bin_data),2)
 gamma_rating = int(''.join(gamma_rating),2)
 epsilon_rating = int(''.join(epsilon_rating),2)
+
 print('Power: ' + str(gamma_rating * epsilon_rating))
 print("Life Support Rating: " + str(oxy_rating * co2_rating))
