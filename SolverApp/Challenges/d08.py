65a7a4aa86cd247de185d1283d76f42e856f9898
adam clark <mr.adam.clark@gmail.com>
2021-12-08 23:18:09 -0700

    found a better way then to make a case statement. starting idea of p2.

--- SolverApp/Challenges/d08.py
+++ SolverApp/Challenges/d08.py
@@ -1,41 +1,37 @@
 #!/usr/bin/env python
 
+digit_map = ['abcefg', 'cf', 'acdeg','acdfg','bcdf','abdfg','abdfge','acf','abcdefg','acbdfg']
+known = {2:1,4:4,3:7,7:8}
+
 class d08:
     def __init__(self):
         pass
 
     def solve_p1(self, signals):
-        digit_counter = [0] * 10
+        total = 0
         for s in signals:
             digits = s[1].split()
             for d in digits:
-                match len(d):
-                    case 2:
-                        digit_counter[1] += 1
-                        continue
-                    case 4:
-                        digit_counter[4] += 1
-                        continue
-                    case 3:
-                        digit_counter[7] += 1
-                        continue
-                    case 7:
-                        digit_counter[8] += 1
-                        continue
-        
-        for i in range(0, len(digit_counter)):
-            print('{} : {}'.format(i, digit_counter[i]))
-            
-        print(sum(digit_counter))   
-        
+                if (len(d) in [2,3,4,7]):
+                    total += 1
+        print(total)
+    
+    def solve_p2(self, data):
+       
+        for d in data:
+            signals = [''.join(sorted(p)) for p in d[0].split()]
+            digits =  [''.join(sorted(p)) for p in d[1].split()]
+            print(signals, digits)
+                    
 
     def solve(self, data):
         signals = [s.split('|') for s in data]
         self.solve_p1(signals)
+        self.solve_p2(signals)
        
 
 if __name__ == '__main__':
-    with open('../../Files/08.input.txt') as file:
+    with open('../../Files/08.test-simple.txt') as file:
             data = [line.strip() for line in file]
     solver = d08()
     solver.solve(data) 
